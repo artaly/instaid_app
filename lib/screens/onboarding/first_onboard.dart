65cf91a5fc9cd6646e9b6166c6b4d11fceac3956
artaly <villalonalycah@gmai.com>
2022-05-06 00:08:00 +0800

    Add first onboard screen code

--- lib/screens/onboarding/first_onboard.dart
+++ lib/screens/onboarding/first_onboard.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+class Onboarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          itemCount: 3,
+          itemBuilder: (_, i) {
+            return Column(
+              children: [],
+            );
+          }),
+    );
+  }
+}
