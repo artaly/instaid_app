ee7855e12408de2a9bcc1daa5953c251f1063279
artaly <villalonalycah@gmai.com>
2022-05-31 22:58:03 +0800

    Add temp onboarding screens

---
+++ lib/screens/onboarding/onboard.dart
@@ -0,0 +1,114 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'content_model.dart';
+
+class Onboarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  int currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+                controller: _pageController,
+                itemCount: contents.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                itemBuilder: (_, i) {
+                  return Padding(
+                    padding: const EdgeInsets.all(40),
+                    child: Column(
+                      children: [
+                        SvgPicture.asset(
+                          contents[i].image,
+                          height: 280,
+                        ),
+                        Text(
+                          contents[i].title,
+                          style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          contents[i].description,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                  contents.length, (index) => buildDot(index, context)),
+            ),
+          ),
+          Container(
+            height: 60,
+            margin: EdgeInsets.all(40),
+            width: double.infinity,
+            color: Colors.white,
+            child: TextButton(
+              child: Text(
+                  currentIndex == contents.length - 1 ? "Continue" : "Next"),
+              onPressed: () {
+                if (currentIndex == contents.length - 1) {}
+                _pageController.nextPage(
+                  duration: Duration(milliseconds: 100),
+                  curve: Curves.bounceIn,
+                );
+              },
+              style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container buildDot(int index, BuildContext context) {
+    return Container(
+        height: 10,
+        width: currentIndex == index ? 25 : 10,
+        margin: EdgeInsets.only(right: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Theme.of(context).primaryColor,
+        ));
+  }
+}
