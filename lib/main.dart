471ff106a7c7f03fc6afafe61a839411e4d928cb
artaly <villalonalycah@gmai.com>
2022-06-20 23:18:23 +0800

    Change primary color

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:instaid_dev/screens/onboarding/onboard.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  Color primaryColorRed = Color(0xffDE4E37);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'InstAID',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: primaryColorRed,
+          primaryColor: HexColor("#699EAD"),
           visualDensity: VisualDensity.adaptivePlatformDensity),
       home: Onboarding(),
     );
