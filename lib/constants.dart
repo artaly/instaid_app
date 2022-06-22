580348a680efd0e580f89a4c7770e53d1068d344
artaly <villalonalycah@gmai.com>
2022-06-22 14:25:15 +0800

    Add two more constant colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const primaryColorBlue = Color(0xFF699EAD);
 const primaryColorRed = Color(0xFFDE4E37);
+const primaryDark = Color(0xFF3F3F3F);
+const primaryBackground = Color(0xFFF6F7FB);
