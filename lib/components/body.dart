fcda7a11cd3a7e77fbac3fb86f14bad9353855b5
artaly <villalonalycah@gmail.com>
2022-06-22 18:12:58 +0800

    Fix body again

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instaid_dev/components/background.dart';
 import 'package:instaid_dev/components/rounded_input_field.dart';
+import 'package:instaid_dev/components/rounded_password_field.dart';
 import 'package:instaid_dev/components/text_field_container.dart';
 import 'package:instaid_dev/constants.dart';
 
@@ -35,35 +36,6 @@ class Body extends StatelessWidget {
           onChanged: (value) {},
         )
       ],
-    ));
-  }
-}
-
-class RoundedPasswordField extends StatelessWidget {
-  final ValueChanged<String> onChanged;
-  const RoundedPasswordField({
-    Key? key,
-    required this.onChanged,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFieldContainer(
-        child: TextField(
-      obscureText: true,
-      onChanged: onChanged,
-      decoration: InputDecoration(
-        hintText: "Password",
-        icon: Icon(
-          Icons.lock,
-          color: primaryColorBlue,
-        ),
-        suffixIcon: Icon(
-          Icons.visibility,
-          color: primaryColorBlue,
-        ),
-        border: InputBorder.none,
-      ),
     ));
   }
 }
