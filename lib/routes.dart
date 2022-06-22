7392da2877e973916a090bc82c195fd2e2b27a57
artaly <villalonalycah@gmail.com>
2022-06-22 22:46:35 +0800

    Add routes code

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:instaid_dev/screens/login/login.dart';
+import 'package:flutter/widgets.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  LoginScreen.routeName: (context) => LoginScreen(),
+};
--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instaid_dev/components/body.dart';
 
 class LoginScreen extends StatelessWidget {
+  static String routeName = "/login";
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
