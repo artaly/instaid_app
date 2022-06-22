c6e6bb99b8eb8666a28ca4d4bb2139a099cab268
artaly <villalonalycah@gmail.com>
2022-06-22 23:19:56 +0800

    test login

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -7,6 +7,10 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Body());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Uhm"),
+      ),
+    );
   }
 }
