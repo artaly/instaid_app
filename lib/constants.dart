5a789a57e593144bb5d6cbdd946cd4eb7ed5a843
artaly <villalonalycah@gmail.com>
2022-06-22 17:51:26 +0800

    Add new color

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,4 @@ const primaryColorBlue = Color(0xFF699EAD);
 const primaryColorRed = Color(0xFFDE4E37);
 const primaryDark = Color(0xFF3F3F3F);
 const primaryBackground = Color(0xFFF6F7FB);
+const primaryColorLBlue = Color(0xFF465156);
