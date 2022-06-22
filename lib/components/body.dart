d20138d2ab225c9ce76ecb32a599f6a39c6cd2eb
artaly <villalonalycah@gmail.com>
2022-06-22 17:57:55 +0800

    Fix body component

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instaid_dev/components/background.dart';
+import 'package:instaid_dev/components/rounded_input_field.dart';
 import 'package:instaid_dev/constants.dart';
 
 class Body extends StatelessWidget {
@@ -31,55 +32,5 @@ class Body extends StatelessWidget {
         )
       ],
     ));
-  }
-}
-
-class RoundedInputField extends StatelessWidget {
-  final String hintText;
-  final IconData icon;
-  final ValueChanged<String> onChanged;
-  const RoundedInputField({
-    Key? key,
-    required this.hintText,
-    this.icon = Icons.person,
-    required this.onChanged,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFieldContainer(
-        child: TextField(
-      onChanged: onChanged,
-      decoration: InputDecoration(
-          hintText: "Your email",
-          border: InputBorder.none,
-          icon: Icon(
-            icon,
-            color: primaryColorLBlue,
-          )),
-    ));
-  }
-}
-
-class TextFieldContainer extends StatelessWidget {
-  final Widget child;
-  const TextFieldContainer({
-    Key? key,
-    required this.child,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      width: size.width * 0.8,
-      decoration: BoxDecoration(
-        color: primaryColorBlue,
-        borderRadius: BorderRadius.circular(29),
-      ),
-      child: child,
-    );
   }
 }
