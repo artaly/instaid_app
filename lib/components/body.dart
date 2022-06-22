a755fb2c717ea89347454468e71a1a42556160bf
artaly <villalonalycah@gmail.com>
2022-06-22 17:51:53 +0800

    Add textfield

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instaid_dev/components/background.dart';
+import 'package:instaid_dev/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({
@@ -8,9 +12,74 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Background(
         child: Column(
-      children: <Widget>[],
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "LOGIN",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        SvgPicture.asset(
+          "assets/svg/first_onboard.svg",
+          height: size.height * 0.35,
+        ),
+        RoundedInputField(
+          hintText: "Your email",
+          onChanged: (value) {},
+        )
+      ],
     ));
+  }
+}
+
+class RoundedInputField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedInputField({
+    Key? key,
+    required this.hintText,
+    this.icon = Icons.person,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+        child: TextField(
+      onChanged: onChanged,
+      decoration: InputDecoration(
+          hintText: "Your email",
+          border: InputBorder.none,
+          icon: Icon(
+            icon,
+            color: primaryColorLBlue,
+          )),
+    ));
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: primaryColorBlue,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: child,
+    );
   }
 }
