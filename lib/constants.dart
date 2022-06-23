b1066d9962fbebd3216563d1f6cbdd05cced7c77
artaly <villalonalycah@gmail.com>
2022-06-24 00:01:39 +0800

    Add form error consts

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,9 @@ const primaryBackground = Color(0xFFF6F7FB);
 const primaryColorLBlue = Color(0xFF465156);
 
 const textColor = Color(0xFF757575);
+
+// For form errors
+
+const String txtEmailNullErr = "Please enter your email";
+const String txtInvalidEmailErr = "Please enter valid email";
+const String txtPassNullErr = "Please enter your password";
