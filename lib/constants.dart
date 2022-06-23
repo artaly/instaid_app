ed0dff02989c2a948c886098d9b3ff7c99409ee2
artaly <villalonalycah@gmail.com>
2022-06-23 21:48:26 +0800

    Add text color const

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,5 @@ const primaryColorRed = Color(0xFFDE4E37);
 const primaryDark = Color(0xFF3F3F3F);
 const primaryBackground = Color(0xFFF6F7FB);
 const primaryColorLBlue = Color(0xFF465156);
+
+const textColor = Color(0xFF757575);
