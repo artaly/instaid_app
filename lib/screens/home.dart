5c153d55c0a6a3d64ed5b7f5875d386c079cc642
artaly <villalonalycah@gmail.com>
2022-06-24 19:29:10 +0800

    Add route

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
+  static String routeName = "/home";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
