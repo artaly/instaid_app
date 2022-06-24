275e316d7d41166fce7beb9d9574e4cd63697388
artaly <villalonalycah@gmail.com>
2022-06-24 18:13:50 +0800

    Add register screen to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:instaid_dev/screens/login/login.dart';
 import 'package:instaid_dev/screens/login/forgot_password.dart';
+import 'package:instaid_dev/screens/register/register.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
   ForgotPassword.routeName: (context) => ForgotPassword(),
+  RegisterScreen.routeName: (context) => RegisterScreen(),
 };
