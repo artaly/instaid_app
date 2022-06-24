fcc12f6c407b279128a3098e3ea75790e66a1d38
artaly <villalonalycah@gmail.com>
2022-06-24 17:15:42 +0800

    Add forgot password to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,10 @@
+import 'dart:js';
+
 import 'package:instaid_dev/screens/login/login.dart';
+import 'package:instaid_dev/screens/login/forgot_password.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
+  ForgotPassword.routeName: (context) => ForgotPassword(),
 };
