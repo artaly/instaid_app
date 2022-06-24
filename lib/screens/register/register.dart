e65857adf57406abc70eeb4e9e5770cb7f39c0ce
artaly <villalonalycah@gmail.com>
2022-06-24 18:13:02 +0800

    Add route name

--- lib/screens/register/register.dart
+++ lib/screens/register/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
+  static String routeName = "/register";
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
