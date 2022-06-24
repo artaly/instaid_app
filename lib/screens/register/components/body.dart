ed4c33bd758310874c5fa84c79a8852bec9d8cb8
artaly <villalonalycah@gmail.com>
2022-06-24 19:28:10 +0800

    Delete unused imports

--- lib/screens/register/components/body.dart
+++ lib/screens/register/components/body.dart
@@ -1,23 +1,36 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:instaid_dev/constants.dart';
 import 'package:instaid_dev/size_config.dart';
+
+import 'register_form.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          "Register Account",
-          style: TextStyle(
-            fontSize: getProportionateScreenWidth(28),
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-            height: 1.5,
-          ),
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenWidth(20),
         ),
-      ],
+        child: Column(
+          children: [
+            Text(
+              "Register Account",
+              style: headingStyle,
+            ),
+            Text(
+              "Complete the following details in\norder to open an account",
+              textAlign: TextAlign.center,
+            ),
+            RegisterForm(),
+          ],
+        ),
+      ),
     );
   }
 }
