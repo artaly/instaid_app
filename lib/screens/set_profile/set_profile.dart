acd5c247158825de0162422effa5079e5cbe9896
artaly <villalonalycah@gmail.com>
2022-06-24 22:42:55 +0800

    Add setup profile

---
+++ lib/screens/set_profile/set_profile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SetupProfile extends StatelessWidget {
+  static String routeName = "/setup_profile";
+  const SetupProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
