c420c68018174f1e3412631c70e153b05b6ff732
artaly <villalonalycah@gmail.com>
2022-06-24 18:29:03 +0800

    Add text

--- lib/screens/register/components/body.dart
+++ lib/screens/register/components/body.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:instaid_dev/size_config.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Text(
+          "Register Account",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(28),
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            height: 1.5,
+          ),
+        ),
+      ],
+    );
   }
 }
