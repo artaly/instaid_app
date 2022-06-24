5dd7ccf2524fd2a1bd99e8c125f5e29d6d19786c
artaly <villalonalycah@gmail.com>
2022-06-24 22:27:39 +0800

    Add safe area

--- lib/screens/register/components/body.dart
+++ lib/screens/register/components/body.dart
@@ -11,24 +11,37 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: getProportionateScreenWidth(20),
-        ),
-        child: Column(
-          children: [
-            Text(
-              "Register Account",
-              style: headingStyle,
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(20),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.02),
+                Text(
+                  "Register Account",
+                  style: headingStyle,
+                ),
+                Text(
+                  "Complete the following details in\norder to open an account",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.07),
+                RegisterForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.07),
+                //SizedBox(height: getProportionateScreenHeight(20)),
+                Text(
+                  'By continuing your confirm that you agree \nwith our Term and Condition',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              ],
             ),
-            Text(
-              "Complete the following details in\norder to open an account",
-              textAlign: TextAlign.center,
-            ),
-            RegisterForm(),
-          ],
+          ),
         ),
       ),
     );
