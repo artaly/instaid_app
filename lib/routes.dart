42562bf8bdcbb4af67f757a936157b658e590a01
artaly <villalonalycah@gmail.com>
2022-06-24 19:18:56 +0800

    Add home route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:instaid_dev/screens/login/login.dart';
 import 'package:instaid_dev/screens/login/forgot_password.dart';
 import 'package:instaid_dev/screens/register/register.dart';
+import 'package:instaid_dev/screens/home.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
   ForgotPassword.routeName: (context) => ForgotPassword(),
   RegisterScreen.routeName: (context) => RegisterScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
 };
