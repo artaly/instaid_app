2b9660edd7e1c2d1fd56380160bc31003120d5e1
artaly <villalonalycah@gmail.com>
2022-06-24 18:29:22 +0800

    Idk

--- lib/screens/register/register.dart
+++ lib/screens/register/register.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../register/components/body.dart';
+
 class RegisterScreen extends StatelessWidget {
   static String routeName = "/register";
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Body(),
+    );
   }
 }
