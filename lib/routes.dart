721451052e1301625e3e0c9d41886182666395cd
artaly <villalonalycah@gmail.com>
2022-06-24 22:46:01 +0800

    Add setup profile route

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:instaid_dev/screens/login/login.dart';
 import 'package:instaid_dev/screens/login/forgot_password.dart';
 import 'package:instaid_dev/screens/register/register.dart';
 import 'package:instaid_dev/screens/home.dart';
+import 'package:instaid_dev/screens/set_profile/set_profile.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -9,4 +10,5 @@ final Map<String, WidgetBuilder> routes = {
   ForgotPassword.routeName: (context) => ForgotPassword(),
   RegisterScreen.routeName: (context) => RegisterScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
+  SetupProfile.routeName: (context) => SetupProfile(),
 };
