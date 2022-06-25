626c5d823e20c563c85a889ee1fb1cdaee540390
artaly <villalonalycah@gmail.com>
2022-06-25 18:52:27 +0800

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:instaid_dev/routes.dart';
@@ -9,7 +10,9 @@ import 'package:instaid_dev/constants.dart';
 import 'package:instaid_dev/screens/login/login.dart';
 import 'package:instaid_dev/constants.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
