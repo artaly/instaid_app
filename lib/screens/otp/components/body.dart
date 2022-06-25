6983a80ad13f103547b83c7d1bc6ff5866928c24
artaly <villalonalycah@gmail.com>
2022-06-25 15:50:12 +0800

    Add body

---
+++ lib/screens/otp/components/body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instaid_dev/size_config.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
