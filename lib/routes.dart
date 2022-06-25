3a5383db50caa81d19466f11d5b6cb5ccc11af94
artaly <villalonalycah@gmail.com>
2022-06-25 15:53:12 +0800

    Add otp route

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,7 @@ import 'package:instaid_dev/screens/login/forgot_password.dart';
 import 'package:instaid_dev/screens/register/register.dart';
 import 'package:instaid_dev/screens/home.dart';
 import 'package:instaid_dev/screens/set_profile/set_profile.dart';
+import 'package:instaid_dev/screens/otp/otp.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -11,4 +12,5 @@ final Map<String, WidgetBuilder> routes = {
   RegisterScreen.routeName: (context) => RegisterScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   SetupProfile.routeName: (context) => SetupProfile(),
+  OTPScreen.routeName: (context) => OTPScreen(),
 };
