ae3ed5b29517888d4cb9e9d79dde2142dc27e453
artaly <villalonalycah@gmail.com>
2022-06-25 15:50:29 +0800

    Add body class

---
+++ lib/screens/otp/otp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:instaid_dev/screens/otp/components/body.dart';
+
+class OTPScreen extends StatelessWidget {
+  static String routeName = "/login";
+  const OTPScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OTP Verification"),
+      ),
+      body: Body(),
+    );
+  }
+}
