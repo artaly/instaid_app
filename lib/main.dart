152f8daa19c3a315d833884d736072012c7858bd
artaly <villalonalycah@gmail.com>
2022-06-26 22:39:17 +0800

    Test commit on another pc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-
+// Test commit
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
