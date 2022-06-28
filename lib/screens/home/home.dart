66249aeccb33968e3f023c06b581e5d4d4564677
artaly <villalonalycah@gmail.com>
2022-06-28 23:58:17 +0800

    Test navbar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instaid_dev/utils/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
@@ -6,7 +7,24 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text("Home Pager"),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(vertical: 14),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 15),
+              blurRadius: 20,
+              color: primaryColor,
+            ),
+          ],
+        ),
+        child: SafeArea(child: Row(children: [])),
       ),
     );
   }
