d1721237bec2b3ab3ccd407b59562f9a945f9adf
artaly <villalonalycah@gmail.com>
2022-06-28 22:22:29 +0800

    Add utils, and colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF699EAD);
+const primaryColorRed = Color(0xFFDE4E37);
+const primaryDark = Color(0xFF3F3F3F);
+const primaryBackground = Color(0xFFF6F7FB);
+const primaryColorLBlue = Color(0xFF465156);
+
+const textColor = Color(0xFF757575);
