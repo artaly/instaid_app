19e9ffcbb5280e7833803ce086e3f8ed0f33454a
artaly <villalonalycah@gmail.com>
2022-06-29 23:50:53 +0800

    Import custom navbar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instaid_dev/utils/colors.dart';
+import 'package:instaid_dev/components/custom_bottom_navbar.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
@@ -8,6 +10,8 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
+      backgroundColor: primaryBackground,
+      bottomNavigationBar: customBottomNavBar(),
     );
   }
 }
