ed6ead7a48e86c8a1a791a69b515c48a071c0d6f
artaly <villalonalycah@gmail.com>
2022-06-29 23:47:48 +0800

    Delete prev navbar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:instaid_dev/utils/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
@@ -7,24 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Pager"),
-      ),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(vertical: 14),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(40),
-            topRight: Radius.circular(40),
-          ),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 15),
-              blurRadius: 20,
-              color: primaryColor,
-            ),
-          ],
-        ),
-        child: SafeArea(child: Row(children: [])),
+        title: Text("Home Page"),
       ),
     );
   }
