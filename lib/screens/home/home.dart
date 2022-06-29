64b4e1e385760af482cdbc568df6e3c7a2388536
artaly <villalonalycah@gmail.com>
2022-06-30 00:55:47 +0800

    Try convert to stateful widget

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,8 +7,28 @@ import 'package:instaid_dev/utils/enum.dart';
 
 import '../../components/custom_bottom_navbar.dart';
 
-class HomeScreen extends StatelessWidget {
+// class HomeScreen extends StatelessWidget {
+//   static String routeName = "/home";
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text("Home Page"),
+//         ),
+//         backgroundColor: primaryBackground,
+//         bottomNavigationBar: CustomNavBar());
+//   }
+// }
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
   static String routeName = "/home";
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
