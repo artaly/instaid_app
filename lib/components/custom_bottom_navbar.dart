de87fb599bf8b8a37b872d47f92847ff318f47db
artaly <villalonalycah@gmail.com>
2022-06-30 01:00:46 +0800

    Add ontap and index

--- lib/components/custom_bottom_navbar.dart
+++ lib/components/custom_bottom_navbar.dart
@@ -5,8 +5,21 @@ import 'package:flutter/material.dart';
 import '../size_config.dart';
 import '../utils/colors.dart';
 
-class CustomNavBar extends StatelessWidget {
+class CustomNavBar extends StatefulWidget {
   const CustomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<CustomNavBar> createState() => _CustomNavBarState();
+}
+
+class _CustomNavBarState extends State<CustomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +32,9 @@ class CustomNavBar extends StatelessWidget {
         selectedItemColor: primaryColor,
         unselectedItemColor: Color(0XFF686868),
         selectedFontSize: 14,
+        currentIndex: _selectedIndex,
         unselectedFontSize: 14,
-        onTap: (value) {
-          // Respond to item press.
-        },
+        onTap: _onItemTapped,
         // ignore: prefer_const_literals_to_create_immutables
         items: [
           BottomNavigationBarItem(
@@ -43,3 +55,44 @@ class CustomNavBar extends StatelessWidget {
     );
   }
 }
+
+
+
+// class CustomNavBar extends StatelessWidget {
+//   const CustomNavBar({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     SizeConfig().init(context);
+//     return SizedBox(
+//       height: SizeConfig.screenHeight * 0.09,
+//       child: BottomNavigationBar(
+//         type: BottomNavigationBarType.fixed,
+//         backgroundColor: Color(0xFFF4F5F9),
+//         selectedItemColor: primaryColor,
+//         unselectedItemColor: Color(0XFF686868),
+//         selectedFontSize: 14,
+//         unselectedFontSize: 14,
+//         onTap: (value) {
+//           // Respond to item press.
+//         },
+//         // ignore: prefer_const_literals_to_create_immutables
+//         items: [
+//           BottomNavigationBarItem(
+//             label: 'Home',
+//             icon: Icon(Icons.home),
+//           ),
+//           BottomNavigationBarItem(
+//             label: 'Contatcs',
+//             icon: Icon(Icons.contact_phone_rounded),
+//           ),
+//           BottomNavigationBarItem(
+//             label: 'Location',
+//             icon: Icon(Icons.location_pin),
+//           ),
+//           BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.person)),
+//         ],
+//       ),
+//     );
+//   }
+// }
