cb668ca188d5cd63d779141e934e05a451c37044
artaly <villalonalycah@gmail.com>
2022-06-30 21:17:26 +0800

    Add temporary panic button

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:instaid_dev/size_config.dart';
 
 import '../../../components/no_account.dart';
+import '../../../utils/colors.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text("huy"),
-    ));
+    SizeConfig().init(context);
+    return _buildEmergencyButton();
+  }
+
+  Material _buildEmergencyButton() {
+    return Material(
+      elevation: 20,
+      type: MaterialType.button,
+      color: primaryColorRed,
+      borderRadius: BorderRadius.circular(1000),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: primaryColorRed, width: 2.0),
+          borderRadius: BorderRadius.circular(1000),
+        ),
+        child: GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Icon(
+              Icons.sensors_rounded,
+              color: Colors.white,
+              size: SizeConfig.screenWidth * 0.3,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
