9f96478234179c5cbc7202d67cef3c5d0394f555
artaly <villalonalycah@gmail.com>
2022-06-30 18:46:19 +0800

    Add body

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,17 +6,20 @@ import 'package:instaid_dev/utils/colors.dart';
 import 'package:instaid_dev/utils/enum.dart';
 
 import '../../components/custom_bottom_navbar.dart';
+import '../../components/default_button.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-        ),
-        backgroundColor: primaryBackground,
-        bottomNavigationBar: CustomNavBar());
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      backgroundColor: primaryBackground,
+      bottomNavigationBar: CustomNavBar(),
+      body: Body(),
+    );
   }
 }
 
