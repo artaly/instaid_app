8e82146bbcaf201f374e79fd744f6f6753e0c690
artaly <villalonalycah@gmail.com>
2022-06-30 18:48:41 +0800

    Add body class

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:instaid_dev/size_config.dart';
+
+import '../../../components/no_account.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
