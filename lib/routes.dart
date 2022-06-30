b1f45fadd9b50c3240324fdb801502188739032e
artaly <villalonalycah@gmail.com>
2022-06-30 20:04:32 +0800

    Add main screen to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:instaid_dev/screens/login/login.dart';
 import 'package:instaid_dev/screens/login/forgot_password.dart';
+import 'package:instaid_dev/screens/main_screen.dart';
 import 'package:instaid_dev/screens/register/register.dart';
 import 'package:instaid_dev/screens/home/home.dart';
 import 'package:instaid_dev/screens/otp/otp.dart';
+import 'package:instaid_dev/screens/main_screen.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -11,4 +13,5 @@ final Map<String, WidgetBuilder> routes = {
   RegisterScreen.routeName: (context) => RegisterScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   OTPScreen.routeName: (context) => OTPScreen(),
+  MainScreen.routeName: (context) => MainScreen(),
 };
