19469d333afc7287fa8d26db2a0984ad6e3050a4
artaly <villalonalycah@gmail.com>
2022-06-30 20:05:39 +0800

    Add main screen

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:instaid_dev/screens/home/home.dart';
+import 'package:instaid_dev/screens/login/login.dart';
+import 'package:instaid_dev/screens/otp/otp.dart';
+
+import '../size_config.dart';
+import '../utils/colors.dart';
+import '../utils/dialogs.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+  static String routeName = "/main_screen";
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late PageController _pageController;
+  int _page = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return WillPopScope(
+      onWillPop: () => Dialogs().showExitDialog(context),
+      child: Scaffold(
+        body: PageView(
+          physics: NeverScrollableScrollPhysics(),
+          controller: _pageController,
+          onPageChanged: onPageChanged,
+          children: <Widget>[HomeScreen(), OTPScreen(), LoginScreen()],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFFF4F5F9),
+          selectedItemColor: primaryColor,
+          unselectedItemColor: Color(0XFF686868),
+          elevation: 20,
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contact_phone_rounded),
+              label: 'Contacts',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.location_pin),
+              label: 'Location',
+            ),
+            BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.person)),
+          ],
+          onTap: navigationTapped,
+          currentIndex: _page,
+        ),
+      ),
+    );
+  }
+
+  void navigationTapped(int page) {
+    _pageController.jumpToPage(page);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._page = page;
+    });
+  }
+}
